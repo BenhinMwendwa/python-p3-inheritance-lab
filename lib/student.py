dcd7fa9b9a7fdb5e18805688210ad68f9d045db1
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-05-30 16:19:43 +0300

    lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
+    def __init__(self,knowledge,first_name, last_name):
+        self.knowledge = knowledge
+        super().__init__(first_name, last_name)
+        knowledge=[]
+        
+    def learn(self,str):
+        self.knowledge.append(str)
         pass
--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
 
 class User:
-    pass
+    def user(self,first_name,last_name):
+       self.first_name = first_name
+       self.last_name = last_name
+        
+    
